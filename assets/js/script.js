ded82d5e09bcd0f3a14c8c677ad234eb94987bcc
namnguyen191 <38139823+namnguyen191@users.noreply.github.com>
2019-12-20 11:15:40 -0500

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,15 @@ function openPage(url) {
 	history.pushState(null, null, url);
 }
 
+function createPlaylist(username){
+	var alert = prompt("Please enter the name of your playlist");
+	if (alert != null) {
+		$.post("includes/handlers/ajax/createPlaylist.php",{name: alert, username: username}).done(function(){
+			//do something when Ajax returns
+			openPage("yourMusic.php");
+		});
+	}
+}
 
 function formatTime(seconds) {
 	var time = Math.round(seconds);
