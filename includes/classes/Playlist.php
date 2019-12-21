6c0c9dbf13794db2a8e619a5826cfe55546918c2
namnguyen191 <38139823+namnguyen191@users.noreply.github.com>
2019-12-20 21:38:19 -0500

    Finnishing all playlist function

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -7,6 +7,11 @@
     private $owner;
 
 		public function __construct($con, $data) {
+			//If the $data parameter is the playlist Id instead of the playlist data
+			if(!is_array($data)){
+				$query = mysqli_query($con,"SELECT * FROM playlists WHERE id ='$data'");
+				$data = mysqli_fetch_array($query);
+			}
 			$this->con = $con;
       $this->id = $data['id'];
       $this->name = $data['name'];
@@ -24,6 +29,25 @@
     public function getOwner(){
       return $this->owner;
     }
+
+		public function getNumberOfSongs(){
+			$query = mysqli_query($this->con, "SELECT songId FROM playlistSongs WHERE playlistId = '$this->id'");
+			return mysqli_num_rows($query);
+		}
+
+		public function getSongIds() {
+
+			$query = mysqli_query($this->con, "SELECT songId FROM playlistSongs WHERE playlistId='$this->id' ORDER BY playlistOrder ASC");
+
+			$array = array();
+
+			while($row = mysqli_fetch_array($query)) {
+				array_push($array, $row['songId']);
+			}
+
+			return $array;
+
+		}
 
 	}
 ?>
---
+++ includes/handlers/ajax/deletePlaylist.php
@@ -0,0 +1,13 @@
+<?php
+
+include '../../config.php';
+if (isset($_POST['playlistId'])) {
+  $playlistId = $_POST['playlistId'];
+  $playlistQuery = mysqli_query($con, "DELETE FROM playlists WHERE id='$playlistId'");
+  $songQuery = mysqli_query($con, "DELETE FROM playlistSongs WHERE playlistId='$playlistId'");
+} else {
+  echo "Playlist id was not passsed into deletePlaylist.php";
+}
+
+
+ ?>
